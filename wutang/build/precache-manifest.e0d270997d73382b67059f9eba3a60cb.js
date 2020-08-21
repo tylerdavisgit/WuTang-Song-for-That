aeed3ebc3850d9c0aaa00d4ede9e8a693cfcfdb0
Tyler Davis <tylerdavis@Tylers-iMac-2.local>
2020-08-21 16:27:20 -0400

    Maybe fixed issues.

---
+++ wutang/build/precache-manifest.e0d270997d73382b67059f9eba3a60cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9ead8abf5300b6a20911e2c7600ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383740911b3cf3481d41",
+    "url": "/static/css/main.fbe13a84.chunk.css"
+  },
+  {
+    "revision": "5a49f95ba7a0a3090f6f",
+    "url": "/static/js/2.4c72ed61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4c72ed61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383740911b3cf3481d41",
+    "url": "/static/js/main.9da2b4f9.chunk.js"
+  },
+  {
+    "revision": "edae2c4bd9dc0d9c3c55",
+    "url": "/static/js/runtime-main.910bf6a8.js"
+  }
+]);
